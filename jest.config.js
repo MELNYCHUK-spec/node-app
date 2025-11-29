027a1c1b19a87c9782c295cfa732d70c44966c34
MELNYCHUK OLEKSII <1265whp@gmail.com>
2025-11-29 17:04:51 +0200

    update

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-    testEnvironment: 'node',
-    testTimeout: 10000,
-    verbose: true,
-    collectCoverage: false,
+  testEnvironment: "node",
+  testTimeout: 10000,
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["index.js", "!node_modules/**", "!coverage/**"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  testResultsProcessor: undefined,
 };
