4142439745b92da1d3eee32ceb73b8c9e61936b2
MELNYCHUK OLEKSII <1265whp@gmail.com>
2025-12-07 09:35:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+const cors = require("cors");
+require("dotenv").config();
+
+// Configure ports
+const args = process.argv;
+const p_index = args.indexOf("--p");
+const cp_index = args.indexOf("--cp");
+const PORT = p_index !== -1 ? args[p_index + 1] : process.env.PORT || 5000;
+const CLIENT_PORT =
+  cp_index !== -1 ? args[cp_index + 1] : process.env.CLIENT_PORT || 3000;
+
+const app = express();
+app.use(
+  cors({
+    origin: `http://localhost:${CLIENT_PORT}`,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  }),
+);
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = new sqlite3.Database("softserve.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to database.");
+});
+
 // CREATE
 app.post("/api/products", (req, res) => {
   const { name, price } = req.body;
-  if (typeof name === 'undefined' || price === undefined) {
+  if (typeof name === "undefined" || price === undefined) {
     res.status(400).json({ error: "Invalid data format" });
     return;
   }
